4bc698727ee922a02b31bd5c7508a437a5975224
VivekPrajapati1123 <137069008+VivekPrajapati1123@users.noreply.github.com>
2023-08-08 09:50:10 +0530

    08-08

---
+++ simplelinklist.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void display()
+{
+    struct node *ptr = head;
+    if(head=NULL)
+    printf("List of empty...");
+    else
+    {
+        while(ptr != NULL)
+        {
+            printf("%d ",ptr->data);
+            ptr=ptr->next;
+        }
+    }
+    printf("\n");
+}
+void insert(int val)
+{
+    struct node *ptr=head;
+    struct node *temp=malloc(sizeof(struct node));
+    temp->data=val;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+        return;
+    }
+    while(ptr->next != NULL)
+    ptr=ptr->next;
+    ptr->next=temp;
+    return;
+}
+void delete()
+{
+    struct node *ptr=head;
+    struct node *p;
+    if(head->next==NULL)
+    {
+        head=NULL;
+        free(ptr);
+        return;
+    }
+    while(ptr->next != NULL)
+    {
+        p=ptr;
+        ptr=ptr->next;
+    }
+    p->next=NULL;
+    free(ptr);
+    return;
+}
+int main()
+{
+    insert(100);
+    insert(200);
+    insert(300);
+    insert(400);
+    display();
+    delete();
+    delete();
+    display();
+}
